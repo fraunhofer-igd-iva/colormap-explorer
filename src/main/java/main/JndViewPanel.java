cff6ca6d6aef0a17f00ccfa534d585fff6db6833
Martin Steiger <martin.steiger@igd.fraunhofer.de>
2014-07-04 17:12:11 +0200

    Added isoline view

--- src/main/java/main/JndViewPanel.java
+++ src/main/java/main/JndViewPanel.java
@@ -20,9 +20,16 @@ package main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.color.ColorSpace;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -35,6 +42,7 @@ import org.terasology.math.geom.Vector2d;
 
 import colormaps.Colormap2D;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.eventbus.Subscribe;
 
@@ -64,6 +72,8 @@ public class JndViewPanel extends JPanel
 	
 	private Voronoi delaunay;
 	
+	private Point curPos;
+
 	public JndViewPanel()
 	{
 		// get last selection event and trigger it manually to be up to date
@@ -72,6 +82,32 @@ public class JndViewPanel extends JPanel
 		{
 			onSelect(selEvent);
 		}
+		
+		addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseExited(MouseEvent e)
+			{
+				curPos = null;
+			}
+		});
+		
+		addMouseMotionListener(new MouseMotionListener()
+		{
+			@Override
+			public void mouseMoved(MouseEvent e)
+			{
+				curPos = new Point(e.getX(), e.getY());
+				JndViewPanel.this.repaint();
+			}
+			
+			@Override
+			public void mouseDragged(MouseEvent e)
+			{
+				curPos = new Point(e.getX(), e.getY());
+				JndViewPanel.this.repaint();
+			}
+		});
 
 		MyEventBus.getInstance().register(this);
 	}
@@ -80,8 +116,8 @@ public class JndViewPanel extends JPanel
 	public void onSelect(ColormapSelectionEvent event)
 	{
 		colormap = event.getSelection();
-		updateRegions();
-		updateDelaunay();
+//		updateRegions();
+//		updateDelaunay();
 		repaint();
 	}
 
@@ -137,7 +173,62 @@ public class JndViewPanel extends JPanel
 		Graphics2D g = (Graphics2D)g1;
 		
 		drawColormap(g);
-		drawVoronoi(g);
+//		drawVoronoi(g);
+		drawJndRegion(g);
+	}
+
+	private void drawJndRegion(Graphics2D g)
+	{
+		if (curPos == null)
+			return;
+		
+		double mx = screenXtoMapX(curPos.getX());
+		double my = screenYtoMapY(curPos.getY());
+		
+		Color color = colormap.getColor((float)mx, (float)my);
+		PColor pcolor = PColor.create(COLOR_SPACE, color.getColorComponents(new float[3]));
+		
+		int angleSteps = 64;
+		double sampleRateDist = 0.005;
+		
+		List<Vector2d> pts = Lists.newArrayList();
+		
+		for (int i = 0; i < angleSteps; i++)
+		{
+			double dx = Math.cos(i * 2.0 * Math.PI / angleSteps);
+			double dy = Math.sin(i * 2.0 * Math.PI / angleSteps);
+			
+			double jndDist = 0;
+			double mapDist = 0;
+			Vector2d best = new Vector2d(mx, my);
+			
+			while (true)
+			{
+				mapDist += sampleRateDist;
+				
+				double px = mx + dx * mapDist; 
+				double py = my + dy * mapDist;
+				
+				if (px < 0 || px > 1 || py < 0 || py > 1)
+					break;
+				
+				Color tcolor = colormap.getColor((float)px, (float)py);
+				PColor ptcolor = PColor.create(COLOR_SPACE, tcolor.getColorComponents(new float[3]));
+
+				jndDist = ColorTools.distance(pcolor, ptcolor, VIEW_ENV);
+				
+				if (jndDist < 2.0)
+					best = new Vector2d(px, py); 
+				else 
+					break;
+			}
+			
+			pts.add(best);
+		}
+		
+		Polygon poly = createPolygon(pts);
+		g.setColor(Color.BLACK);
+		g.draw(poly);
 	}
 
 	private int getScreenWidth()
@@ -184,12 +275,7 @@ public class JndViewPanel extends JPanel
 		}
 	}
 	
-	/**
-	 * Draws Voronoi diagram based on current triangulation
-	 * A Voronoi diagram can be created from a Delaunay triangulation by
-	 * connecting the circumcenters of neighboring triangles
-	 */
-	private void drawVoronoi(Graphics g)
+	private void drawVoronoiOutline(Graphics2D g)
 	{
 		List<LineSegment> segs = delaunay.voronoiDiagram();
 
@@ -203,15 +289,70 @@ public class JndViewPanel extends JPanel
 			
 			g.drawLine((int)x1, (int)y1, (int)x2, (int)y2);
 		}
+	}
+	
+	/**
+	 * Draws Voronoi diagram based on current triangulation
+	 * A Voronoi diagram can be created from a Delaunay triangulation by
+	 * connecting the circumcenters of neighboring triangles
+	 */
+	private void drawVoronoiRegions(Graphics2D g)
+	{
+		for (final Vector2d center : delaunay.siteCoords())
+		{
+			List<Vector2d> corners = Lists.newArrayList(delaunay.region(center));
+			
+            Collections.sort(corners, new Comparator<Vector2d>() {
+
+                @Override
+                public int compare(Vector2d o0, Vector2d o1) {
+                	Vector2d a = new Vector2d(o0).sub(center).normalize();
+                    Vector2d b = new Vector2d(o1).sub(center).normalize();
+
+                    if (a.y() > 0) { //a between 0 and 180
+                        if (b.y() < 0) //b between 180 and 360
+                            return -1;
+                        return a.x() < b.x() ? 1 : -1;
+                    } else { // a between 180 and 360
+                        if (b.y() > 0) //b between 0 and 180
+                            return 1;
+                        return a.x() > b.x() ? 1 : -1;
+                    }
+                }
+            });
+            
+            Color color = colormap.getColor((float)center.getX(), (float)center.getY());
+            g.setColor(color);
+            
+            Polygon poly = createPolygon(corners);
+            g.fill(poly);
+        }
 
 		g.setColor(Color.WHITE);
 		for (Vector2d pt : delaunay.siteCoords())
 		{
-			int r = 2;
+			int r = 1;
 			int wx = (int) mapXtoScreenX(pt.getX());
-			int wy = (int) mapXtoScreenX(pt.getY());
+			int wy = (int) mapYtoScreenY(pt.getY());
 			g.fillOval(wx - r, wy - r, 2 * r , 2 * r);
 		}
 	}
+
+	 private Polygon createPolygon(List<Vector2d> pts) 
+	 {
+        int[] x = new int[pts.size()];
+        int[] y = new int[pts.size()];
+        
+        for (int i = 0; i < pts.size(); i++) 
+        {
+			int wx = (int) mapXtoScreenX(pts.get(i).getX());
+			int wy = (int) mapYtoScreenY(pts.get(i).getY());
+            x[i] = wx;
+            y[i] = wy;
+        }
+        
+        return new Polygon(x, y, pts.size());
+    }
+
 
 }
