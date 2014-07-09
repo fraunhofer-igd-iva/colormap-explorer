fde64992c573ab6223fb773307157192912d3dbf
Simon Thum <simon.thum@gmx.de>
2014-07-10 00:33:50 +0200

    add median-based lines to analysis view

--- src/main/java/main/AnalysisPanel.java
+++ src/main/java/main/AnalysisPanel.java
@@ -16,9 +16,17 @@
 
 package main;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.hypot;
+import static java.lang.Math.sin;
+
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -39,7 +47,71 @@ public class AnalysisPanel extends JPanel
 {
 	private static final long serialVersionUID = 4842610449905121603L;
 
-	private final List<ColormapPanel> panels = Lists.newArrayList();
+	private final List<MismatchScatterplotPanel> panels = Lists.newArrayList();
+	
+	private int lines = 10000;
+	
+	// list of lines across which to compare as pairs of pairs of floats
+	List<Float> makeLinePoints() {
+		List<Float> list = new ArrayList<>(lines*4);
+		Random r = new Random(123456);
+		
+		for (int i = 0; i < lines; i++)
+		{
+			float ax = r.nextFloat();
+			float ay = r.nextFloat();
+			
+			float bx = 0, by = 0;
+			
+			final float dist = r.nextFloat();
+			
+			int tries = 10;
+			while (tries >= 1) {
+				float angle = (float) (r.nextFloat() * Math.PI * 2.0);
+				
+				bx = (float) (ax + sin(angle) * dist); 
+				by = (float) (ay + cos(angle) * dist);
+				if (bx >= 0.0 && bx <= 1.0 && by >= 0.0 && by <= 1.0)
+					break;
+				tries --;
+			}
+			if (tries == 0)
+				continue;  // should not happen often, distorts scatterplot
+			list.add(ax);
+			list.add(ay);
+			list.add(bx);
+			list.add(by);
+		}
+		return list;
+	}
+	
+	double deriveMedianColormapToJNDRatio(Colormap2D colormap, Iterator<Float> floats, int lines) {
+		if (lines == 0)
+			return Double.NaN;
+		double[] ratios = new double[lines];
+	
+		for (int i = 0; i < lines; i++)
+		{
+			float ax = floats.next();
+			float ay = floats.next();
+			float bx = floats.next();
+			float by = floats.next();
+	
+			float dist = (float) hypot(ax-bx, ay-by);
+			
+			Color colorA = colormap.getColor(ax, ay);
+			Color colorB = colormap.getColor(bx, by);
+			
+			// roughly 0-100
+			double cdist = MismatchScatterplotPanel.colorDiff(colorA, colorB);
+			
+			double ratio = cdist / dist;
+			ratios[i] = ratio;
+		}
+		Arrays.sort(ratios);
+		return ratios[lines/2];
+	}
+
 	
 	/**
 	 * Default constructor
@@ -74,8 +146,12 @@ public class AnalysisPanel extends JPanel
 	public void onSelect(ColormapSelectionEvent event)
 	{
 		Colormap2D colormap = event.getSelection();
-		for (ColormapPanel panel : panels)
+		List<Float> linePoints = makeLinePoints();
+		int actualLines = linePoints.size()/4;
+		double median = deriveMedianColormapToJNDRatio(colormap, linePoints.iterator(), actualLines);
+		for (MismatchScatterplotPanel panel : panels)
 		{
+			panel.setPointSource(linePoints, actualLines, median);
 			panel.setColormap(colormap);
 		}
 	}
--- src/main/java/main/MismatchScatterplotPanel.java
+++ src/main/java/main/MismatchScatterplotPanel.java
@@ -20,6 +20,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 import javax.swing.JPanel;
@@ -29,7 +31,6 @@ import de.fhg.igd.pcolor.PColor;
 import de.fhg.igd.pcolor.colorspace.CS_sRGB;
 import de.fhg.igd.pcolor.colorspace.ViewingConditions;
 import de.fhg.igd.pcolor.util.ColorTools;
-import static java.lang.Math.*;
 
 /**
  * Displays a scatterplot of perceptual color distance over color map distance.
@@ -41,6 +42,8 @@ public class MismatchScatterplotPanel extends JPanel implements ColormapPanel
 {
 	private static final Color NORMAL_DOT_COLOR = new Color(0, 0, 0, 0.1f);
 
+	private static final Color MEDIAN_LINE_COLOR = new Color(0, 0, 0, 0.8f);
+
 	private static final Color BORDERLINE_DOT_COLOR = new Color(1,0,0,0.5f);
 	
 
@@ -50,9 +53,13 @@ public class MismatchScatterplotPanel extends JPanel implements ColormapPanel
 
 	private Colormap2D colormap;
 
-	private int points = 50000;
+	private int lines = 0;
 	
 	private boolean useLog;
+
+	private List<Float> floats;
+
+	private double medianRatio;
 	
 	/**
 	 * @param colormap the colormap for the points
@@ -75,11 +82,13 @@ public class MismatchScatterplotPanel extends JPanel implements ColormapPanel
 	}
 	
 	/**
-	 * @param numPoints the number of points
+	 * @param num the number of points
 	 */
-	public void setPointNumber(int numPoints)
+	public void setPointSource(List<Float> floats, int num, double medianRatio)
 	{
-		this.points = numPoints;
+		this.lines = num;
+		this.floats = floats;
+		this.medianRatio = medianRatio;
 	}
 	
 	@Override
@@ -88,7 +97,6 @@ public class MismatchScatterplotPanel extends JPanel implements ColormapPanel
 		super.paintComponent(g1);
 		Graphics2D g = (Graphics2D)g1;
 		
-		Random r = new Random(123456);
 		int dia = 3;
 
 		double maxX = getWidth() - dia;
@@ -96,28 +104,17 @@ public class MismatchScatterplotPanel extends JPanel implements ColormapPanel
 
 		Object oldAAhint = g.getRenderingHint(RenderingHints.KEY_ANTIALIASING);
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		Iterator<Float> flt = floats.iterator();
 
-		for (int i = 0; i < points; i++)
+		for (int i = 0; i < lines; i++)
 		{
-			float ax = r.nextFloat();
-			float ay = r.nextFloat();
+			float ax = flt.next();
+			float ay = flt.next();
+			float bx = flt.next();
+			float by = flt.next();
+			float dist = (float) Math.hypot(ax-bx, ay-by);
 			
-			float bx = 0, by = 0;
-			
-			final float dist = r.nextFloat();
-			
-			int tries = 10;
-			while (tries >= 1) {
-				float angle = (float) (r.nextFloat() * Math.PI * 2.0);
-				
-				bx = (float) (ax + sin(angle) * dist); 
-				by = (float) (ay + cos(angle) * dist);
-				if (bx >= 0.0 && bx <= 1.0 && by >= 0.0 && by <= 1.0)
-					break;
-				tries --;
-			}
-			if (tries == 0)
-				continue;  // should not happen often, distorts scatterplot
 			
 			Color colorA = colormap.getColor(ax, ay);
 			Color colorB = colormap.getColor(bx, by);
@@ -132,26 +129,42 @@ public class MismatchScatterplotPanel extends JPanel implements ColormapPanel
 				g.setColor(NORMAL_DOT_COLOR);
 			
 			// normalize
-			cdist /= 120f;  
+			cdist /= medianRatio;
 			
 			int xCoord = (int)(maxX * dist);
 			int yCoord;
 			if (useLog)
-				yCoord = (int)((Math.log(cdist / dist)/3)*maxY + (maxY/2));
+				yCoord = (int)((Math.log(cdist / dist)/Math.log(2))*maxY + (maxY/2));
 			else
-				yCoord = (int)(maxY * cdist);
+				yCoord = (int)(maxY * cdist * 2 / 3);
 			g.fillOval(xCoord, yCoord, dia, dia);
 		}
 		
+		
+		if (useLog) {
+			g.setColor(NORMAL_DOT_COLOR);
+			int yc = (int)((Math.log(2)/Math.log(2))*maxY + (maxY/2));
+			g.drawLine(0, yc, (int)maxX, yc);
+			yc = (int)((Math.log(1/2f)/Math.log(2))*maxY + (maxY/2));
+			g.drawLine(0, yc, (int)maxX, yc);
+			g.setColor(MEDIAN_LINE_COLOR);
+			yc = (int)(maxY/2);
+			g.drawLine(0, yc, (int)maxX, yc);
+		} else {
+			g.setColor(MEDIAN_LINE_COLOR);
+			g.drawLine(0, 0, (int)maxX, (int)(maxY * 2 / 3));
+		}
 		g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, oldAAhint);
 	}
 	
-	private PColor convert(Color color) {
+	public static PColor convert(Color color) {
 		return PColor.create(CS_sRGB.instance, color.getColorComponents(new float[3]));
 	}
 	
-	private double colorDiff(Color c1, Color c2) {
+	public static double colorDiff(Color c1, Color c2) {
 		return ColorTools.distance(convert(c1), convert(c2), comparisonVc);
 	}
+	
+
 
 }
