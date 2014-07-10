0e9d7d27cb1ef58d590d7cd9b1002c32299b5ac6
Martin Steiger <martin.steiger@igd.fraunhofer.de>
2014-07-10 09:55:47 +0200

    Bilinear interpolation

--- src/main/java/colormaps/CachedColormap2D.java
+++ src/main/java/colormaps/CachedColormap2D.java
@@ -23,6 +23,8 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
+import com.google.common.base.Preconditions;
+
 /**
  * A cached, but otherwise transparent {@link Colormap2D} 
  * wrapper implementation.
@@ -61,17 +63,54 @@ public class CachedColormap2D extends DelegateColormap2D
 	@Override
 	public Color getColor(float mx, float my)
 	{
-		// TODO: consider using bi-linear interpolation between the four neighboring pixels
+		Preconditions.checkArgument(0 <= mx && mx <= 1, "X must in in range [0..1], but is %s", mx);
+		Preconditions.checkArgument(0 <= my && my <= 1, "Y must in in range [0..1], but is %s", my);
+
+		double x = mx * (img.getWidth() - 1);
+		double y = my * (img.getHeight() - 1);
 		
-		int x = (int) (mx * (img.getWidth() - 1) + 0.5);
-		int y = (int) (my * (img.getHeight() - 1) + 0.5);
-		
-		int rgb = img.getRGB(x, y);
-		Color color = new Color(rgb);
-		
-		return color;
+		int minX = (int)Math.floor(x);
+        int maxX = (int)Math.ceil(x);
+
+        int minY = (int)Math.floor(y);
+        int maxY = (int)Math.ceil(y);
+
+        Color q00 = new Color(img.getRGB(minX, minY));
+        Color q10 = new Color(img.getRGB(maxX, minY));
+        Color q01 = new Color(img.getRGB(minX, maxY));
+        Color q11 = new Color(img.getRGB(maxX, maxY));
+
+        double ipx = x - minX;
+        double ipy = y - minY;
+
+        Color color = bilerp(q00, q10, q01, q11, ipx, ipy);
+
+        return color;
 	}
 
+	private static Color bilerp(Color xt1, Color xt2, Color xb1, Color xb2, double p, double ipy) 
+	{
+		float[] arrt1 = xt1.getColorComponents(new float[3]);
+		float[] arrt2 = xt2.getColorComponents(new float[3]);
+		
+		double rt = arrt1[0] * (1.0 - p) + arrt2[0] * p;
+		double gt = arrt1[1] * (1.0 - p) + arrt2[1] * p;
+		double bt = arrt1[2] * (1.0 - p) + arrt2[2] * p;
+
+		float[] arrb1 = xb1.getColorComponents(new float[3]);
+		float[] arrb2 = xb2.getColorComponents(new float[3]);
+		
+		double rb = arrb1[0] * (1.0 - p) + arrb2[0] * p;
+		double gb = arrb1[1] * (1.0 - p) + arrb2[1] * p;
+		double bb = arrb1[2] * (1.0 - p) + arrb2[2] * p;
+
+		double r = rt * (1.0 - ipy) + rb * ipy;
+		double g = gt * (1.0 - ipy) + gb * ipy;
+		double b = bt * (1.0 - ipy) + bb * ipy;
+
+		return new Color((float)r, (float)g, (float)b);
+    }
+	
 	/**
 	 * @return the cached image
 	 */
